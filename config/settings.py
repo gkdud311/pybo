c00b9e2f61cee8492c77e125ef9f6ec6a15bf4fe
김하영 <gkdud311@naver.com>
2022-02-17 23:38:38 +0900

    수정

--- config/settings.py
+++ config/settings.py
@@ -83,7 +83,9 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD': 'zaqxsw159',
         'PORT': '3306',
-        'OPTIONS': {'charset': 'utf8mb4'},
+        'OPTIONS': {'charset': 'utf8mb4',
+                    'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     }
 }
 
